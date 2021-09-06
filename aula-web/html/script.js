708a98219c4dff617e4f1075d57630aa58a64f88
henriquekirchheck <henriquekirchheck@gmail.com>
2021-09-06 16:08:13 -0300

    Aninhated List *WIP*

--- aula-web/html/script.js
+++ aula-web/html/script.js
@@ -2,8 +2,8 @@ const conteudo = document.querySelector('section#conteudo')
 
 document.querySelectorAll('a').forEach(link => {
     link.onclick = function(e) {
+        e.preventDefault()
         if (link.getAttribute('show') !== null) {
-            e.preventDefault()
 
             fetch(link.href)
                 .then(resp => resp.text())
