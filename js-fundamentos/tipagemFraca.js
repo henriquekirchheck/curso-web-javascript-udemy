31ef314f7660b5a0e8d04ab24fdb3fd431b6b221
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-02-05 11:54:14 -0300

    Tipagem fraca

---
+++ js-fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genericos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // produto quimico perigoso 
