3d4a5e59f6fb12ded94f6c33ea7bcf9388dbc830
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-02-07 17:02:36 -0300

    Quase tudo é função

---
+++ js-fundamentos/funcaoEverywere.js
@@ -0,0 +1,10 @@
+class Produto {}
+function Prod() {}
+
+console.log(typeof Object)
+console.log(typeof Array)
+console.log(typeof Number)
+console.log(typeof String)
+console.log(typeof Boolean)
+console.log(typeof Produto)
+console.log(typeof Prod)
