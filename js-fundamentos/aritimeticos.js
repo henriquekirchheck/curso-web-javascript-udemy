d5a9bf97a688e088355e4f426ef0c915e2408aa7
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-02-09 16:55:34 -0300

    Operadores Aritimeticos

---
+++ js-fundamentos/aritimeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = a - b - c - d
+const multiplicacao = a * b * c * d
+const divisao = a / b / c / d
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
